f4afa4f65a0597aca73d5a5eb714c683c8d0606f
Administrator <Rahman2Sazal@github.com>
2026-08-03 23:24:38 +0200

    string updates 2

--- StringTest.js
+++ StringTest.js
@@ -4,3 +4,10 @@ console.log(fname.toUpperCase());
 console.log(fname.toLowerCase());
 console.log(fname.charAt(0));
 console.log(fname[0]);
+console.log(fname.indexOf("f"));
+console.log(fname.split(""));
+
+let arr=fname.split(" ");
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
