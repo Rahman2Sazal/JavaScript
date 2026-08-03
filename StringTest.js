c8be725d5e0196db171e47b3baa511c8e0bc23c2
Administrator <Rahman2Sazal@github.com>
2026-08-03 23:43:20 +0200

    lesson 3 done

--- StringTest.js
+++ StringTest.js
@@ -1,13 +1,25 @@
-let fname = "Mahfuz";
+let fname = "Mahfuzur Rahman";
 console.log(fname.length);
 console.log(fname.toUpperCase());
 console.log(fname.toLowerCase());
-console.log(fname.charAt(0));
+console.log(fname.charAt(0));   
 console.log(fname[0]);
 console.log(fname.indexOf("f"));
+console.log((fname.trim( )));   
 console.log(fname.split(""));
 
 let arr=fname.split(" ");
 for(let i=0; i<arr.length; i++){
     console.log(arr[i]);
 }
+
+console.log(fname.trim( ));  
+
+let str = "Hello World 616";
+console.log(str);
+let value=str.split(" ") [2];
+console.log(value);
+console.log(typeof value);
+let num = parseInt(value);
+console.log(num);
+console.log(typeof num);    
